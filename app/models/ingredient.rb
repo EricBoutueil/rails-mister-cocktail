d88bb720f229557e15efe3a21526becb96eda5f4
Eric Boutueil <e.boutueil@gmail.com>
2018-02-15 15:03:41 +0100

    models created and crash tested with page saved :)

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ApplicationRecord
   has_many :doses
   has_many :cocktails, through: :doses
+  accepts_nested_attributes_for :doses
+
   validates :name, uniqueness: true, presence: true
 end
